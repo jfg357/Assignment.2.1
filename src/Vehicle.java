d5b680156b661a6013c28494dccfc5d0a2627f3a
Juan Gaviria <jfg357@gmail.com>
2018-10-20 18:59:12 -0400

    Create the Vehicle class

---
+++ src/Vehicle.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Vehicle implements Engine, Chassis{
+
+  @Override
+  public Chassis getChassisType() {
+    return null;
+  }
+
+  @Override
+  public void setChassisType(String vehicleChassis) {
+
+  }
+
+  @Override
+  public void setEngineCylinders(int engineCylinders) {
+
+  }
+
+  @Override
+  public void setEngineManufacturedDate(Date date) {
+
+  }
+
+  @Override
+  public void setEngineManufacturer(String manufacturer) {
+
+  }
+
+  @Override
+  public void setEngineMake(String engineMake) {
+
+  }
+
+  @Override
+  public void setEngineModel(String engineModel) {
+
+  }
+
+  @Override
+  public void setDriveTrain(String driveTrain) {
+
+  }
+
+  @Override
+  public void setEngineType(String fuel) {
+
+  }
+}
