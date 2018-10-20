21ff52e25ab73058142ad76fd4718e109f5a0c37
Juan Gaviria <jfg357@gmail.com>
2018-10-20 18:56:01 -0400

    Create the ExteriorFeature class

---
+++ src/ExteriorFeature.java
@@ -0,0 +1,12 @@
+public class ExteriorFeature implements Feature{
+
+  @Override
+  public String getFeature() {
+    return null;
+  }
+
+  @Override
+  public void setFeature(String feature) {
+
+  }
+}
