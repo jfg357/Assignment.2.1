3c822569c05a65b2b203bd4179da2e0c7cdbe496
Juan Gaviria <jfg357@gmail.com>
2018-10-20 19:00:13 -0400

    Implement constructors

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,10 @@
+import java.util.*;
+
 public class Car extends Vehicle{
 
 
+  public Car(Date date, String honda, String prelude, String coupe, VehicleChassis unibody, String aNull, String type, ManufacturedEngine manufacturedEngine, Feature[] f,
+      int i) {
+    super();
+  }
 }
