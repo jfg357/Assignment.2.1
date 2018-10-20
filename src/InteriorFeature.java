787b20303467c2605bd372e91443510eb65db92c
Juan Gaviria <jfg357@gmail.com>
2018-10-20 18:54:59 -0400

    Create the InteriorFeature class

---
+++ src/InteriorFeature.java
@@ -0,0 +1,15 @@
+public class InteriorFeature implements Feature {
+
+  public InteriorFeature(String s) {
+  }
+
+  @Override
+  public String getFeature() {
+    return null;
+  }
+
+  @Override
+  public void setFeature(String feature) {
+
+  }
+}
