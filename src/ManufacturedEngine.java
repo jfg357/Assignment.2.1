06076f2390d3e713121a0c0ddfbb26acd3d6e934
Juan Gaviria <jfg357@gmail.com>
2018-10-20 18:59:31 -0400

    Implement constructors

--- src/ManufacturedEngine.java
+++ src/ManufacturedEngine.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class ManufacturedEngine implements Engine{
 
+  public ManufacturedEngine(String honda, Date date, String s, String h23A1, String s1, int i, String s2) {
+  }
+
   @Override
   public void setEngineCylinders(int engineCylinders) {
 
