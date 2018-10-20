2664882e755c03a00ce0e23fdc1db8797ad19a05
Juan Gaviria <jfg357@gmail.com>
2018-10-20 18:51:57 -0400

    Create the ManufacturedEngine class and implement the interface

--- src/ManufacturedEngine.java
+++ src/ManufacturedEngine.java
@@ -1,3 +1,39 @@
-public class ManufacturedEngine {
+import java.util.*;
 
+public class ManufacturedEngine implements Engine{
+
+  @Override
+  public void setEngineCylinders(int engineCylinders) {
+
+  }
+
+  @Override
+  public void setEngineManufacturedDate(Date date) {
+
+  }
+
+  @Override
+  public void setEngineManufacturer(String manufacturer) {
+
+  }
+
+  @Override
+  public void setEngineMake(String engineMake) {
+
+  }
+
+  @Override
+  public void setEngineModel(String engineModel) {
+
+  }
+
+  @Override
+  public void setDriveTrain(String driveTrain) {
+
+  }
+
+  @Override
+  public void setEngineType(String fuel) {
+
+  }
 }
