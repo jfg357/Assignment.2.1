23a9b52e892a0aedeae28e8b4256f007959d1401
Juan Gaviria <jfg357@gmail.com>
2018-10-20 18:01:13 -0400

    Create concrete class VehicleFrame

---
+++ src/VehicleFrame.java
@@ -0,0 +1,12 @@
+public class VehicleFrame implements Chassis {
+
+  @Override
+  public Chassis getChassisType() {
+    return null;
+  }
+
+  @Override
+  public void setChassisType(String vehicleChassis) {
+
+  }
+}
