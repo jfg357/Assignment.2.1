be3dcbee824e3f26fa34a03985681c09f1c0c989
Juan Gaviria <jfg357@gmail.com>
2018-10-20 23:37:16 -0400

    Format fixing

--- src/Car.java
+++ src/Car.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
-public final class Car extends Vehicle{
+public final class Car extends Vehicle {
 
   private Feature[] feature = new Feature[10];
   private int carAxle;
 
-  public Car(){
+  public Car() {
     super();
 
     Feature[] feature = {new InteriorFeature("No Interior Features"),
@@ -34,6 +34,46 @@ public final class Car extends Vehicle{
     this.carAxle = carAxle;
   }
 
+  public String getExteriorFeatures() {
+    String list = "";
+    for (int i = 0; i < this.feature.length; i++) {
+      if (this.feature[i] instanceof ExteriorFeature) {
+        if (list.length() == 0) {
+          list += this.feature[i];
+        } else {
+          list += "\n" + this.feature[i];
+        }
+      }
+    }
+    return list;
+  }
 
 
+  public String getInteriorFeatures() {
+    String list = "";
+    for (int i = 0; i < this.feature.length; i++) {
+      if (this.feature[i] instanceof InteriorFeature) {
+        if (list.length() == 0) {
+          list += this.feature[i];
+        } else {
+          list += "\n" + this.feature[i];
+        }
+      }
+    }
+    return list;
+  }
+
+  public String toString() {
+    String list = "";
+    for (int i = 0; i < this.feature.length; i++) {
+      if (list.length() == 0) {
+        list += this.feature[i];
+      }
+      else {
+        list += "\n" + this.feature[i];
+      }
+    }
+    return super.toString() + "\n" + "Features : " + "\n" + list + "\n"
+        + "Car Axle : " + carAxle;
+  }
 }
